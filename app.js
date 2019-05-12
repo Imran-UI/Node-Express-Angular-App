cfb3f44a2eb7bbba155883b68d4f5a78262f526e
Mohammad Imran Shaik -X (mshaik4 - ZENSAR TECHNOLOGIES INC at Cisco) <mshaik4@cisco.com>
2019-05-11 21:12:40 -0700

    setup CORS

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const app = express();
 
 app.use(bodyParser.json()); // for application json
 
+app.use((req,res,next) => {
+    res.setHeader('Access-Controll-Allow-Origin', '*');
+    res.setHeader('Access-Controll-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Controll-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
 
 
 app.use('/feed', feedRoutes);
